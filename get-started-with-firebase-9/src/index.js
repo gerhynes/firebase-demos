b331ef6cb09523840e3ee70be38903bd363b88bd
Gerard Hynes <gkhynes@gmail.com>
2021-11-09 22:23:42 +0000

    Set up firebase project

--- get-started-with-firebase-9/src/index.js
+++ get-started-with-firebase-9/src/index.js
@@ -1,1 +1,12 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpbPV-DhMJfcB5Sv41lqqsm1M515Al7n0",
+  authDomain: "get-started-with-firebase-9.firebaseapp.com",
+  projectId: "get-started-with-firebase-9",
+  storageBucket: "get-started-with-firebase-9.appspot.com",
+  messagingSenderId: "1028649190460",
+  appId: "1:1028649190460:web:b24fa7ffb32a678a3def39"
+};
+
+initializeApp(firebaseConfig);
