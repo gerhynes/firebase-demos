cc74603126eb164696af78d11549fa27b4804a93
Gerard Hynes <gkhynes@gmail.com>
2021-11-10 23:11:14 +0000

    Add demo functions

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -7,3 +7,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   // functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello World");
 });
+
+exports.doubleNumber = functions.https.onRequest((request, response) => {
+  let doubled = `Result is ${parseInt(request.query.data) * 2}`;
+  response.send(doubled);
+});
+
+exports.postComment = functions.https.onRequest((request, response) => {
+  const body = JSON.parse(request.body);
+  body.timestamp = Date.now();
+  const updatedBody = JSON.stringify(body);
+  response.send(updatedBody);
+});
