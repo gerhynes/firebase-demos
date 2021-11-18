338cf9de1a4e750aae9a407ce71bcd0ae565d66a
Gerard Hynes <gkhynes@gmail.com>
2021-11-18 10:12:54 +0000

    Update single document

--- get-started-with-firebase-9/src/index.js
+++ get-started-with-firebase-9/src/index.js
@@ -10,7 +10,8 @@ import {
   where,
   orderBy,
   serverTimestamp,
-  getDoc
+  getDoc,
+  updateDoc
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -77,4 +78,18 @@ getDoc(docRef).then((doc) => {
 
 onSnapshot(docRef, (doc) => {
   console.log(doc.data(), doc.id);
+});
+
+// Updating a document
+const updateForm = document.querySelector(".update");
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", updateForm.id.value);
+
+  updateDoc(docRef, {
+    title: "updated title"
+  }).then(() => {
+    updateForm.reset();
+  });
 });
